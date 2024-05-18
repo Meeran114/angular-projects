9d939d5114c4fff9c344dc4aea8abbef9215e72b
[Mohamed,Meeran] <[meerumeeran191@gmail.com]>
2024-05-18 12:32:54 +0530

    Checking commited

--- home-app/src/app/app-route.ts
+++ home-app/src/app/app-route.ts
@@ -2,16 +2,21 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { DetailsComponent } from "./details/details.component";
 
-const routeconfig : Routes = [{
-    path:'',
-    component : HomeComponent,
-    title:"Home Page"
-},
-{
-    path : 'details/:id',
-    component:DetailsComponent,
-    title:"Details Page"
-}
-]
+const routeconfig: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    title: "Home Page",
+  },
+  {
+    path: "details/:id",
+    component: DetailsComponent,
+    title: "Details Page",
+  },
+  {
+    path: "New Path",
+    component: DetailsComponent,
+  },
+];
 
 export default routeconfig;
